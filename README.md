611bd06723f15e6ab70435b7a4ea05c627c84a62
Moataz Bahaa <86017015+moataz-bahaa@users.noreply.github.com>
2023-04-26 14:32:42 +0200

    schema version 1

---
+++ README.md
@@ -0,0 +1,1 @@
+# Gaming marketplace API
--- app.js
+++ app.js
@@ -2,12 +2,13 @@ import 'dotenv/config';
 import 'express-async-errors';
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 
 const app = express();
 
 // middlewares
 app.use(cors({ origin: '*' }));
-app.use(express.static('public'));
+app.use(express.static(path.resolve('public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
